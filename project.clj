b382d0fdc78e7c6c25b6c399bc37309f43b60ba8
Prashant Sharma <prashant.iiith@gmail.com>
2012-01-18 15:12:46 +0530

    Pallet-Demo

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pallet-collector "1.0.0-SNAPSHOT"
+(defproject pallet-demos "1.0.0-SNAPSHOT"
   :description "Demonstration of various pallet features."
   :dependencies [
 		[org.clojure/clojure "1.2.0"]
